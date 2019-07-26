eab85a8d013187407fad6b38a7cf4eb5caf5e836
Chulki Lee <chulki.lee@gmail.com>
2019-07-26 00:08:22 -0700

    release 0.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Brodex.MixProject do
   def project do
     [
       app: :brodex,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -12,7 +12,10 @@ defmodule Brodex.MixProject do
       description: "A thin wrapper of brod.",
       package: [
         licenses: ["Apache-2.0"],
-        links: %{"GitHub" => "https://github.com/chulkilee/brodex"}
+        links: %{
+          "GitHub" => "https://github.com/chulkilee/brodex",
+          "Changelog" => "https://github.com/chulkilee/brodex/blob/master/CHANGELOG.md"
+        }
       ],
       # docs
       source_url: "https://github.com/chulkilee/brodex"
